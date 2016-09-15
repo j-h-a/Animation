74dd970f25a792649804c47eece695477c907ee6
Jay Abbott <jay@abbott.me.uk>
2016-09-16 00:49:21 +0100

    Initial Commit

---
+++ Animation/Animation.h
@@ -0,0 +1,19 @@
+//
+//  Animation.h
+//  Animation
+//
+//  Created by Jay on 2016-09-16.
+//  Copyright © 2016 Jay Abbott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animation.
+FOUNDATION_EXPORT double AnimationVersionNumber;
+
+//! Project version string for Animation.
+FOUNDATION_EXPORT const unsigned char AnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animation/PublicHeader.h>
+
+
