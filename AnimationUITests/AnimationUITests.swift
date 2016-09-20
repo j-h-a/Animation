5510cce2e86c188fba267ea1f805911124696756
Jay Abbott <jay@abbott.me.uk>
2016-09-20 14:13:22 +0100

    Add (failing) tests for add/remove/update of Animatables

--- AnimationUITests/AnimationUITests.swift
+++ AnimationUITests/AnimationUITests.swift
@@ -25,6 +25,29 @@ class AnimationUITests: XCTestCase {
 		case completion
 	}
 
+	class AnimatableTest: Animatable {
+
+		private(set) var updateCount: Int = 0
+		private var waitForCount: Int = 0
+		private var actionToTake: (() -> Void)? = nil
+
+		func take(action: @escaping () -> Void, after: Int) {
+			updateCount = 0
+			waitForCount = after
+			actionToTake = action
+		}
+
+		func update(by timeInterval: Double) {
+
+			updateCount += 1
+			if(updateCount == waitForCount) {
+				if let action = actionToTake {
+					action()
+				}
+			}
+		}
+	}
+
 	func testAnimationStateProgressionComplete() {
 
 		let animationDone = expectation(description: "animation completed ok")
@@ -270,5 +293,120 @@ class AnimationUITests: XCTestCase {
 		XCTAssertEqual(a2UpdateCount, 2, "retriggered iteration should have updated exactly twice")
 		XCTAssertEqual(a1CompletedCount, 1, "first iteration should have completion called exactly once")
 		XCTAssertEqual(a2CompletedCount, 1, "retriggered iteration should have completion called exactly once")
+	}
+
+	func testAnimatableAddMultiple() {
+
+		let a1 = AnimatableTest()
+		let a2 = AnimatableTest()
+		let a3 = AnimatableTest()
+		let e1 = expectation(description: "animatable 1 being updated")
+		let e2 = expectation(description: "animatable 2 being updated")
+		let e3 = expectation(description: "animatable 3 being updated")
+
+		a1.take(action: { e1.fulfill() }, after: 5)
+		a2.take(action: { e2.fulfill() }, after: 10)
+		a3.take(action: { e3.fulfill() }, after: 15)
+		Animation.add(animatable: a1)
+		Animation.add(animatable: a2)
+		Animation.add(animatable: a3)
+
+		waitForExpectations(timeout: 5)
+	}
+
+	func testAnimatableRemove() {
+
+		let a1 = AnimatableTest()
+		let e1 = expectation(description: "animatable being updated")
+
+		a1.take(action: { e1.fulfill() }, after: 5)
+		Animation.add(animatable: a1)
+		waitForExpectations(timeout: 5)
+
+		// a1 still running - cause failure unless removed
+		a1.take(action: { XCTFail("animation a1 was not removed") }, after: 5)
+
+		let a2 = AnimatableTest()
+		let e2 = expectation(description: "animatable removed")
+		a2.take(
+			action: {
+				Animation.remove(animatable: a1)
+				a2.take(action: { e2.fulfill() }, after: 5)
+			}, after: 2)
+		Animation.add(animatable: a2)
+
+		waitForExpectations(timeout: 5)
+	}
+
+	func testAnimatableAddingOthers() {
+
+		let a1 = AnimatableTest()
+		let a2 = AnimatableTest()
+		let e1 = expectation(description: "animatable added other one")
+
+		a1.take(action: { Animation.add(animatable: a2) }, after: 5)
+		a2.take(action: { e1.fulfill() }, after: 5)
+		Animation.add(animatable: a1)
+
+		waitForExpectations(timeout: 5)
+	}
+
+	func testAnimatableRemovingOthers() {
+
+		let a1 = AnimatableTest()
+		let a2 = AnimatableTest()
+		let a3 = AnimatableTest()
+		let e1 = expectation(description: "animatable removed other one")
+
+		a1.take(action: { XCTFail() }, after: 10)
+		a2.take(action: { Animation.remove(animatable: a1) }, after: 5)
+		a3.take(action: { e1.fulfill() }, after: 20)
+		Animation.add(animatable: a1)
+		Animation.add(animatable: a2)
+		Animation.add(animatable: a3)
+
+		waitForExpectations(timeout: 5)
+	}
+
+	func testAnimatableRemovingSelf() {
+
+		let a1 = AnimatableTest()
+		let a2 = AnimatableTest()
+		let e1 = expectation(description: "animatable removed self")
+
+		a1.take(action: { Animation.remove(animatable: a1); e1.fulfill() }, after: 5)
+		Animation.add(animatable: a1)
+		waitForExpectations(timeout: 5)
+
+		a1.take(action: { XCTFail("animation not removed") }, after: 5)
+
+		let e2 = expectation(description: "fail didn't happen before expectation met")
+		a2.take(action: { e2.fulfill() }, after: 10)
+		Animation.add(animatable: a2)
+		waitForExpectations(timeout: 5)
+	}
+
+	func testAnimatableAddingExisting() {
+
+		let a1 = AnimatableTest()
+		let a2 = AnimatableTest()
+		let e1 = expectation(description: "animatable was updated")
+
+		a2.take(action: { e1.fulfill() }, after: 25)
+
+		Animation.add(animatable: a1)
+		Animation.add(animatable: a2)
+
+		Animation.add(animatable: a1)
+		Animation.add(animatable: a1)
+		Animation.add(animatable: a1)
+		Animation.add(animatable: a1)
+
+		waitForExpectations(timeout: 5)
+
+		Animation.remove(animatable: a2)
+		Animation.remove(animatable: a1)
+
+		XCTAssertEqual(a1.updateCount, a2.updateCount, "both animations should have the same update count")
 	}
 }
