8c846dfa29b6f7ebcbf7b8e6f8ca8b42f5a5db2a
Jay Abbott <jay@abbott.me.uk>
2016-09-21 22:15:16 +0100

    Implement composite curve and add tests for composite curve.

--- Animation/Curve.swift
+++ Animation/Curve.swift
@@ -143,8 +143,255 @@ open class HermiteCurve: Parametric {
 }
 
 open class CompositeCurve: Parametric {
+
+	private let startValue: Double
+	private let segments: [Segment]
+
+	private struct Segment {
+		let endPoint: Double
+		let endValue: Double
+		let curve: Parametric
+	}
+
+	private class func add(segment: Segment, to segments: inout [Segment]) throws {
+
+		let previousEndPoint = segments.last != nil ? segments.last!.endPoint : 0.0
+
+		guard previousEndPoint < 1.0 else {
+			throw ParseError.additionalCurveSegmentsBeyondOne
+		}
+		guard (segment.endPoint > 0.0) && (segment.endPoint <= 1.0) else {
+			throw ParseError.endPointOutOfRange(message: "End point \(segment.endPoint) is outside the valid range: 0.0 < endPoint <= 1.0.")
+		}
+		guard segment.endPoint > previousEndPoint else {
+			throw ParseError.endPointsNotIncreasing(message: "End point \(segment.endPoint), must be bigger than the previous end point \(previousEndPoint)")
+		}
+
+		segments.append(segment)
+	}
+
+	private class func internalInit(format: String, args: [Any]) throws -> (startValue: Double, segments: [Segment]) {
+
+		guard format.characters.count == args.count else {
+			throw ParseError.invalidNumberOfParameters
+		}
+
+		var specifiedStartValue: Double? = nil
+		var tmpSegments = [Segment]()
+
+		var idx: Int = 0
+		for char in format.characters {
+			switch char {
+			case "S": // StartValue
+				guard idx == 0 else {
+					throw ParseError.startValueNotFirst(message: "If present, 'S' must be the first character of the format string. Found at index \(idx).")
+				}
+				guard let inputSV = args[idx] as? Double else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'S' must be a Double.")
+				}
+				specifiedStartValue = inputSV
+			case "L": // Linear
+				guard let input = args[idx] as? (endPoint: Double, endValue: Double) else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'L' must be (endPoint: Double, endValue: Double)")
+				}
+				try CompositeCurve.add(segment: Segment(endPoint: input.endPoint, endValue: input.endValue, curve: Curve.linear), to: &tmpSegments)
+			case "E": // EaseInEaseOut
+				guard let input = args[idx] as? (endPoint: Double, endValue: Double) else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'E' must be (endPoint: Double, endValue: Double)")
+				}
+				try CompositeCurve.add(segment: Segment(endPoint: input.endPoint, endValue: input.endValue, curve: Curve.easeInEaseOut), to: &tmpSegments)
+			case "I": // EaseIn
+				guard let input = args[idx] as? (endPoint: Double, endValue: Double) else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'I' must be (endPoint: Double, endValue: Double)")
+				}
+				try CompositeCurve.add(segment: Segment(endPoint: input.endPoint, endValue: input.endValue, curve: Curve.easeIn), to: &tmpSegments)
+			case "O": // EaseOut
+				guard let input = args[idx] as? (endPoint: Double, endValue: Double) else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'O' must be (endPoint: Double, endValue: Double)")
+				}
+				try CompositeCurve.add(segment: Segment(endPoint: input.endPoint, endValue: input.endValue, curve: Curve.easeOut), to: &tmpSegments)
+			case "H": // Hermite
+				guard let input = args[idx] as? (endPoint: Double, endValue: Double, gradientIn: Double, gradientOut: Double) else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'H' must be (endPoint: Double, endValue: Double, gradientIn: Double, gradientOut: Double)")
+				}
+				let hermite = HermiteCurve(gradientIn: input.gradientIn, gradientOut: input.gradientOut)
+				try CompositeCurve.add(segment: Segment(endPoint: input.endPoint, endValue: input.endValue, curve: hermite), to: &tmpSegments)
+			case "C": // Curve
+				guard let input = args[idx] as? (endPoint: Double, endValue: Double, curve: Parametric) else {
+					throw ParseError.invalidArgumentType(message: "The parameter for 'H' must be (endPoint: Double, endValue: Double, gradientIn: Double, gradientOut: Double)")
+				}
+				try CompositeCurve.add(segment: Segment(endPoint: input.endPoint, endValue: input.endValue, curve: input.curve), to: &tmpSegments)
+			default:
+				throw ParseError.invalidFormatCharacter(message: "'\(char)' is not a valid format character.")
+			}
+
+			idx += 1
+		}
+
+		guard tmpSegments.count > 0 else {
+			throw ParseError.noCurveSegments
+		}
+		guard tmpSegments.last?.endPoint == 1.0 else {
+			throw ParseError.finalEndPointNotOne
+		}
+
+		return (startValue: specifiedStartValue ?? 0.0, segments: tmpSegments)
+	}
+
+
+
+	// MARK: - Public interface
+
+
+
+	public enum ParseError: Error {
+		case invalidNumberOfParameters
+		case invalidFormatCharacter(message: String)
+		case invalidArgumentType(message: String)
+		case startValueNotFirst(message: String)
+		case endPointsNotIncreasing(message: String)
+		case endPointOutOfRange(message: String)
+		case additionalCurveSegmentsBeyondOne
+		case finalEndPointNotOne
+		case noCurveSegments
+	}
+
+	/** Creates a composite curve from a format string with parameters.
+
+	The composite curve is made up from other curve segments, and parameterised
+	from 0 - 1 across all segments. Just like with other curve types, the value
+	of the curve at any point is obtained through Parametric interface.
+
+	The first curve segment starts at 0.0 and ends at a	user-defined 'endPoint'
+	where 0.0 < endPoint <= 1.0. Each subsequent segment starts at the end-point
+	of the previous segment and continues until its own end-point (which must be
+	greater than its start-point). The end-point of the last curve segment must be 1.0.
+
+	Each segment also has a startValue and endValue, these determine the output value
+	of the curve. The startValue is taken implicitly from the endValue for the previous
+	curve segment, except for the first curve segment where it is specified separately
+	(or defaults to 0.0), and each curve segment has an explicit endValue. This is the
+	value of the curve at the end of the segment (i.e. when the input parameter is
+	equal to the endPoint of this segment).
+
+	Typically, if the output value of the curve is to be used for interpolation, then
+	it should be between 0 and 1. For example if the output value is used as the input
+	to another curve, or for a linear interpolation (LERP) done outside of the curve
+	calculation. However, interpolation values outside the range 0 - 1 are still valid
+	and result in extrapolated values, so startValue and endValue for each segment can
+	be increasing, or decreasing, and can be greater than 1 or less than 0, these just
+	determine the output value of the curve.
+
+	A composite curve is constructed from a format string. Each character in the format
+	string represents a segment of the curve (except for 'S' which represents the
+	start-value of the first curve segment), the curve type is determined by the format
+	character and its parameters are passed in as a tuple of values. So for each
+	character in the format string there should be exactly one parameter passed in.
+	The valid format characters and expected parameter types are listed below:
+
+	````
+	Format Character | Parameter Type
+	-----------------+-------------------------------------------------------------
+	S (StartValue)   | Double
+	L (Linear)       | (endPoint: Double, endValue: Double)
+	E (EaseInEaseOut)| (endPoint: Double, endValue: Double)
+	I (EaseIn)       | (endPoint: Double, endValue: Double)
+	O (EaseOut)      | (endPoint: Double, endValue: Double)
+	H (Hermite)      | (endPoint: Double, endValue: Double, gradientIn: Double, gradientOut: Double)
+	C (Curve)        | (endPoint: Double, endValue: Double, curve: Parametric)
+	````
+
+	If the start-value for the first curve segment is specified with 'S', it must be the
+	first item in the format string and must only appear once. It defaults to 0.0 if not
+	specified.
+	
+	Any violations of the requirements will result in a CompoundCurve.ParseError being thrown.
+
+	- parameters:
+	  - format: The format string for creating the curve
+	  - args: The parameters for each character in the format string
+	*/
+	public init(format: String, _ args: Any...) throws {
+		let initData = try CompositeCurve.internalInit(format: format, args: args)
+		startValue = initData.startValue
+		segments = initData.segments
+	}
+
+	/** Creates a composite curve from a format string with an array of parameters.
+
+	Provides a way to initialise a composite curve from a programatically generated array
+	of parameters. The parameters in the array should be the same types and in the same
+	order as the parameters passed to the variadic version of this initialiser.
+
+	- parameters:
+	  - format: The format string for creating the curve
+	  - args: An array containing one parameter for each character in the format string
+
+	- seealso: init(format: String, args: Any...) throws
+	*/
+	public init(format: String, _ args: [Any]) throws {
+		let initData = try CompositeCurve.internalInit(format: format, args: args)
+		startValue = initData.startValue
+		segments = initData.segments
+	}
+
 	public subscript(t: Double) -> Double {
-		// TODO
-		return 0
+
+		// guard segments.count > 0 else { return t } // Segment count cannot be 0
+
+		var activeSegment: Segment
+		var activeSegmentStartValue: Double
+		var localT: Double
+
+		switch t {
+		case _ where segments.count == 1:
+			// Use the only curve segment with a 1:1 mapping for t
+			activeSegment = segments[0]
+			activeSegmentStartValue = startValue
+			localT = t
+			// All other cases have at least 2 segments
+		case _ where t <= 0.0:
+			// Use the first curve segment with t extrapolated downwards
+			activeSegment = segments[0]
+			activeSegmentStartValue = startValue
+			localT = t / activeSegment.endPoint
+		case _ where t >= 1.0:
+			// Use the last curve segment with t expanded to 0.0 - 1.0 between start point and end point
+			let previousSegment = segments[segments.count - 2]
+			let	startPoint = previousSegment.endPoint
+			activeSegment = segments[segments.count - 1]
+			activeSegmentStartValue = previousSegment.endValue;
+			localT = (t - startPoint) / (activeSegment.endPoint - startPoint)
+		default:
+			// Binary search to find the active segment; where previousSegment.endPoint < t <= activeSegment.endPoint
+			var minIdx: Int = 0
+			var maxIdx: Int = segments.count - 1
+			var startPoint: Double
+			var endPoint: Double
+			while(minIdx < maxIdx)
+			{
+				let midIdx = (minIdx + maxIdx) / 2
+				startPoint = (midIdx == 0) ? 0.0 : segments[midIdx - 1].endPoint
+				endPoint = segments[midIdx].endPoint
+				// Check to see if t is between start/end or if we should keep searching
+				if(t <= startPoint) {
+					maxIdx = midIdx - 1 // Search lower
+				} else if(t > endPoint) {
+					minIdx = midIdx + 1 // Search higher
+				} else {
+					minIdx = midIdx; maxIdx = midIdx // We found our match
+				}
+			}
+
+			let activeIdx = minIdx
+
+			// Use the active curve segment with t expanded to 0.0 - 1.0 between start point and end point
+			activeSegment = segments[activeIdx]
+			activeSegmentStartValue = (activeIdx == 0) ? startValue : segments[activeIdx - 1].endValue
+			startPoint              = (activeIdx == 0) ? 0.0        : segments[activeIdx - 1].endPoint
+			localT = (t - startPoint) / (activeSegment.endPoint - startPoint)
+		}
+
+		return activeSegmentStartValue <~~ activeSegment.curve[localT] ~~> activeSegment.endValue
 	}
 }
--- AnimationTests/CurveTests.swift
+++ AnimationTests/CurveTests.swift
@@ -17,12 +17,12 @@ func sameSize(arrays: [[Any]]) -> Bool {
 	return true
 }
 
-let gradientLength = 0.0002
+let gradientLength = 0.00001
 let gradientAccuracy = 0.0006
 
 class CurveTests: XCTestCase {
 
-	func performTests(on curve: Parametric, inputs: [Double], expectedOutputs: [Double], expectedGradients: [Double]) {
+	func doCurveTests(on curve: Parametric, inputs: [Double], expectedOutputs: [Double], expectedGradients: [Double]) {
 		guard sameSize(arrays: [inputs, expectedOutputs, expectedGradients]) else {
 			XCTFail("input arrays should be the same size")
 			return
@@ -39,99 +39,276 @@ class CurveTests: XCTestCase {
 	}
 
 	func testCurveOne() {
-		performTests(on: Curve.one,
+		doCurveTests(on: Curve.one,
 		             inputs:            [-2.1, 0.0, 0.2, 0.5, 0.8, 1.0, 2.3],
 		             expectedOutputs:   [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
 		             expectedGradients: [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 	}
 
 	func testCurveZero() {
-		performTests(on: Curve.zero,
+		doCurveTests(on: Curve.zero,
 		             inputs:            [-2.1, 0.0, 0.2, 0.5, 0.8, 1.0, 2.3],
 		             expectedOutputs:   [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
 		             expectedGradients: [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 	}
 
 	func testCurveLinear() {
-		performTests(on: Curve.linear,
+		doCurveTests(on: Curve.linear,
 		             inputs:            [-1.1, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.5],
 		             expectedOutputs:   [-1.1, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.5],
 		             expectedGradients: [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
 	}
 
 	func testCurveEaseInEaseOut() {
-		performTests(on: Curve.easeInEaseOut,
+		doCurveTests(on: Curve.easeInEaseOut,
 		             inputs:            [-1.6, 0.0, 0.2  , 0.5, 0.8  , 1.0, 2.3],
 		             expectedOutputs:   [ 0.0, 0.0, 0.104, 0.5, 0.896, 1.0, 1.0],
 		             expectedGradients: [ 0.0, 0.0, 0.96 , 1.5, 0.96 , 0.0, 0.0])
 	}
 
 	func testCurveEaseIn() {
-		performTests(on: Curve.easeIn,
+		doCurveTests(on: Curve.easeIn,
 		             inputs:            [-2.1, 0.0, 0.2  , 0.5  , 0.8  , 1.0, 2.3],
 		             expectedOutputs:   [ 0.0, 0.0, 0.072, 0.375, 0.768, 1.0, 2.3],
 		             expectedGradients: [ 0.0, 0.0, 0.68 , 1.25 , 1.28 , 1.0, 1.0])
 	}
 
 	func testCurveEaseOut() {
-		performTests(on: Curve.easeOut,
+		doCurveTests(on: Curve.easeOut,
 		             inputs:            [-2.7, 0.0, 0.2  , 0.5  , 0.8  , 1.0, 2.2],
 		             expectedOutputs:   [-2.7, 0.0, 0.232, 0.625, 0.928, 1.0, 1.0],
 		             expectedGradients: [ 1.0, 1.0, 1.28 , 1.25 , 0.68 , 0.0, 0.0])
 	}
 
 	func testCurveBell() {
-		performTests(on: Curve.bell,
+		doCurveTests(on: Curve.bell,
 		             inputs:            [-1.3, 0.0, 0.1  , 0.4  , 0.5, 0.6  , 0.9  , 1.0, 1.7],
 		             expectedOutputs:   [ 0.0, 0.0, 0.104, 0.896, 1.0, 0.896, 0.104, 0.0, 0.0],
 		             expectedGradients: [ 0.0, 0.0, 1.92 , 1.92 , 0.0,-1.92 ,-1.92 , 0.0, 0.0])
 	}
 
 	func testCurveParabolicAcceleration() {
-		performTests(on: Curve.parabolicAcceleration,
+		doCurveTests(on: Curve.parabolicAcceleration,
 		             inputs:            [-1.4, 0.0, 0.2 , 0.5 , 0.8 , 1.0, 1.9 ],
 		             expectedOutputs:   [ 0.0, 0.0, 0.04, 0.25, 0.64, 1.0, 3.61],
 		             expectedGradients: [ 0.0, 0.0, 0.4 , 1.0 , 1.6 , 2.0, 3.8 ])
 	}
 
 	func testCurveParabolicDeceleration() {
-		performTests(on: Curve.parabolicDeceleration,
+		doCurveTests(on: Curve.parabolicDeceleration,
 		             inputs:            [-1.2 , 0.0, 0.2 , 0.5 , 0.8 , 1.0, 1.1],
 		             expectedOutputs:   [-3.84, 0.0, 0.36, 0.75, 0.96, 1.0, 1.0],
 		             expectedGradients: [ 4.4 , 2.0, 1.6 , 1.0 , 0.4 , 0.0, 0.0])
 	}
 
 	func testCurveParabolicPeak() {
-		performTests(on: Curve.parabolicPeak,
+		doCurveTests(on: Curve.parabolicPeak,
 		             inputs:            [-0.3 , 0.0, 0.2 , 0.4 , 0.5, 0.6 , 0.8, 1.0, 1.4  ],
 		             expectedOutputs:   [-1.56, 0.0, 0.64, 0.96, 1.0, 0.96, 0.64, 0.0,-2.24],
 		             expectedGradients: [ 6.4 , 4.0, 2.4 , 0.8 , 0.0,-0.8 ,-2.4 ,-4.0,-7.2 ])
 	}
 
 	func testCurveParabolicBounce() {
-		performTests(on: Curve.parabolicBounce,
+		doCurveTests(on: Curve.parabolicBounce,
 		             inputs:            [-1.5, 0.0, 0.2 , 0.3 , 0.5, 0.7 , 0.8 , 1.0, 2.5],
 		             expectedOutputs:   [ 0.0, 0.0, 0.16, 0.36, 1.0, 0.36, 0.16, 0.0, 0.0],
 		             expectedGradients: [ 0.0, 0.0, 1.6 , 2.4 , 0.0,-2.4 ,-1.6 , 0.0, 0.0])
 	}
 
 	func testHermiteCurve() {
-		
-		performTests(on: HermiteCurve(gradientIn: 2, gradientOut: 1),
+		doCurveTests(on: HermiteCurve(gradientIn: 2, gradientOut: 1),
 		             inputs:            [-1.0, 0.0, 0.2  , 0.5  , 0.8  , 1.0, 2.0],
 		             expectedOutputs:   [-2.0, 0.0, 0.328, 0.625, 0.832, 1.0, 2.0],
 		             expectedGradients: [ 2.0, 2.0, 1.32 , 0.75 , 0.72 , 1.0, 1.0])
-		performTests(on: HermiteCurve(gradientIn: 0.5, gradientOut: 3),
+		doCurveTests(on: HermiteCurve(gradientIn: 0.5, gradientOut: 3),
 		             inputs:            [-1.0, 0.0, 0.2  , 0.5   , 0.8  , 1.0, 2.0],
 		             expectedOutputs:   [-0.5, 0.0, 0.072, 0.1875, 0.528, 1.0, 4.0],
 		             expectedGradients: [ 0.5, 0.5, 0.28 , 0.625 , 1.78 , 3.0, 3.0])
-		performTests(on: HermiteCurve(gradientIn: 4, gradientOut: 4),
+		doCurveTests(on: HermiteCurve(gradientIn: 4, gradientOut: 4),
 		             inputs:            [-1.0, 0.0, 0.2  , 0.5, 0.8  , 1.0, 2.0],
 		             expectedOutputs:   [-4.0, 0.0, 0.488, 0.5, 0.512, 1.0, 5.0],
 		             expectedGradients: [ 4.0, 4.0, 1.12 ,-0.5, 1.12 , 4.0, 4.0])
-		performTests(on: HermiteCurve(gradientIn: -1, gradientOut: -1),
+		doCurveTests(on: HermiteCurve(gradientIn: -1, gradientOut: -1),
 		             inputs:            [-1.0, 0.0, 0.1  , 0.2  , 0.5, 0.8  , 0.9  , 1.0, 2.0],
 		             expectedOutputs:   [ 1.0, 0.0,-0.044, 0.008, 0.5, 0.992, 1.044, 1.0, 0.0],
 		             expectedGradients: [-1.0,-1.0, 0.08 , 0.92 , 2.0, 0.92 , 0.08 ,-1.0,-1.0])
+	}
+
+	func testCompositeCurveThrowsInvalidNumberOfParameters() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "SL", args: [ 1.0, (endPoint: 1.0, endValue: 0.0),  3.0 ]),
+			(fmt: "SL", args: [ 1.0,                                      ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown invalidNumberOfParameters with format string '\(data.fmt)' and \(data.args.count) parameters")
+			} catch CompositeCurve.ParseError.invalidNumberOfParameters {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+
+	}
+
+	func testCompositeCurveThrowsParameterTypeErrors() {
+		for char in "SLEIOHC".characters {
+			do {
+				try _ = CompositeCurve(format: String(char), "string type not expected")
+				XCTFail("CompositeCurve init should have thrown invalidArgumentType for \(char)")
+			} catch CompositeCurve.ParseError.invalidArgumentType(_) {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsOnInvalidStartValue() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "SLS", args: [ 1.0,                             (endPoint: 1.0, endValue: 0.0),  3.0                            ]),
+			(fmt: "LS" , args: [ (endPoint: 1.0, endValue: 0.0),  1.0                                                             ]),
+			(fmt: "LSL", args: [ (endPoint: 0.5, endValue: 1.0),  0.0,                             (endPoint: 1.0, endValue: 0.0) ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown startValueNotFirst with format string '\(data.fmt)'")
+			} catch CompositeCurve.ParseError.startValueNotFirst(_) {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsInvalidFormatCharacter() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "SLx", args: [ 1.0,                             (endPoint: 1.0, endValue: 0.0),  3.0                            ]),
+			(fmt: "LLy", args: [ (endPoint: 0.5, endValue: 1.0),  (endPoint: 1.0, endValue: 0.0),  1.0                            ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown invalidFormatCharacter with format string '\(data.fmt)'")
+			} catch CompositeCurve.ParseError.invalidFormatCharacter(_) {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsNoCurveSegments() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "",  args: [     ]),
+			(fmt: "S", args: [ 1.0 ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown noCurveSegments with format string '\(data.fmt)'")
+			} catch CompositeCurve.ParseError.noCurveSegments {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsFinalEndPointNotOne() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "SL",  args: [ 1.0,                             (endPoint: 0.5,  endValue: 0.0)                                    ]),
+			(fmt: "LL" , args: [ (endPoint: 0.5, endValue: 0.1),  (endPoint: 0.99, endValue: 1.0)                                    ]),
+			(fmt: "LLL", args: [ (endPoint: 0.5, endValue: 1.0),  (endPoint: 0.75, endValue: 0.0),  (endPoint: 0.999, endValue: 0.5) ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown finalEndPointNotOne")
+			} catch CompositeCurve.ParseError.finalEndPointNotOne {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsAdditionalCurveSegmentsBeyondOne() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "LL" , args: [ (endPoint: 1.0, endValue: 0.5),  (endPoint: 1.0, endValue: 1.0)                                  ]),
+			(fmt: "LLL", args: [ (endPoint: 0.5, endValue: 1.0),  (endPoint: 1.0, endValue: 0.0),  (endPoint: 0.3, endValue: 0.5) ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown additionalCurveSegmentsBeyondOne")
+			} catch CompositeCurve.ParseError.additionalCurveSegmentsBeyondOne {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsEndPointsNotIncreasing() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "LL" , args: [ (endPoint: 0.5, endValue: 0.1),  (endPoint: 0.4, endValue: 1.0)                                  ]),
+			(fmt: "LLL", args: [ (endPoint: 0.5, endValue: 1.0),  (endPoint: 0.6, endValue: 0.0),  (endPoint: 0.6, endValue: 0.5) ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown endPointsNotIncreasing")
+			} catch CompositeCurve.ParseError.endPointsNotIncreasing(_) {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurveThrowsEndPointOutOfRange() {
+		let testData: [(fmt: String, args: [Any])] = [
+			(fmt: "LL" , args: [ (endPoint: -0.5, endValue: 0.1),  (endPoint: 1.0, endValue: 1.0)                                  ]),
+			(fmt: "LLL", args: [ (endPoint:  0.0, endValue: 1.0),  (endPoint: 0.5, endValue: 0.0),  (endPoint: 1.0, endValue: 0.5) ]),
+			(fmt: "LLL", args: [ (endPoint:  0.1, endValue: 1.0),  (endPoint: 1.1, endValue: 0.0),  (endPoint: 1.0, endValue: 0.5) ]),
+			]
+		for data in testData {
+			do {
+				try _ = CompositeCurve(format: data.fmt, data.args)
+				XCTFail("CompositeCurve init should have thrown endPointOutOfRange")
+			} catch CompositeCurve.ParseError.endPointOutOfRange(_) {
+				// Expected error!
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
+	}
+
+	func testCompositeCurve() {
+		let testData: [(fmt: String, args: [Any], i: [Double], eO: [Double], eG: [Double])] = [
+			(fmt: "SEEE" , args: [ 0.5,
+			                       (endPoint: 0.333333333333, endValue: 1.0),
+			                       (endPoint: 0.666666666667, endValue: 0.0),
+			                       (endPoint: 1.0           , endValue: 0.5) ],
+				i:  [-0.5, 0.0, 0.166666666667, 0.333333333333, 0.5, 0.666666666667, 0.833333333333, 1.0, 1.5],
+				eO: [ 0.5, 0.5, 0.75          , 1.0           , 0.5, 0.0           , 0.25          , 0.5, 0.5],
+				eG: [ 0.0, 0.0, 2.25          , 0.0           ,-4.5, 0.0           , 2.25          , 0.0, 0.0]),
+			(fmt: "CL"   , args: [ (endPoint: 0.5, endValue: 1.0, curve: Curve.parabolicAcceleration),
+			                       (endPoint: 1.0, endValue: 0.75) ],
+				i:  [-0.5, 0.0, 0.25, 0.49999, 0.75 , 1.0 , 1.5],
+				eO: [ 0.0, 0.0, 0.25, 0.99996, 0.875, 0.75, 0.5],
+				eG: [ 0.0, 0.0, 2.0 , 3.99999,-0.5  ,-0.5 ,-0.5]),
+			]
+		for data in testData {
+			do {
+				let cCurve = try CompositeCurve(format: data.fmt, data.args)
+				doCurveTests(on: cCurve, inputs: data.i, expectedOutputs: data.eO, expectedGradients: data.eG)
+			} catch {
+				XCTFail("Unexpected error: \(error)")
+			}
+		}
 	}
 }
